b4570abe243e7fdf257d717292b2210ddd7e9a11
Jefferson Nunes <nunesjefferson425@gmail.com>
2024-07-12 20:42:56 -0300

    edições swaggwer.json

--- ROUTES/swaggerRoutes.js
+++ ROUTES/swaggerRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import swaggerUi from'swagger-ui-express'
-import swaggerDocument from '../swagger.json'
+
+import swaggerDocument from '../swagger.json' assert { type: 'json' };
 const swaggerRouter = Router()
 
 swaggerRouter.use('/', swaggerUi.serve)
